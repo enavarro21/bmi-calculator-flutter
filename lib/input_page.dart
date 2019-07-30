369132f68c4d9ec2b62bf21b4dc4e1b666598dcd
Erwin Navarro <erwin.r.navarro1@gmail.com>
2019-07-30 13:16:22 -0700

    Male and Female card selectors with functionality completed

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -6,7 +6,13 @@ import 'reusable_card.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
 const bottomContainerColor = Color(0xFFEB1555);
+
+enum Gender {
+  male,
+  female,
+}
 
 class InputPage extends StatefulWidget {
   @override
@@ -14,6 +20,9 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  
+  Gender selectedGender;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +37,12 @@ class _InputPageState extends State<InputPage> {
               children: <Widget>[
                 Expanded(
                   child: ReusableCard(
-                    color: activeCardColor,
+                    onPress: (){
+                      setState(() {
+                       selectedGender = Gender.male; 
+                      });
+                    },
+                    color: selectedGender == Gender.male ? activeCardColor : inactiveCardColor,
                     cardChild: IconContent(
                       icon: FontAwesomeIcons.mars,
                       label: 'Male',
@@ -36,13 +50,19 @@ class _InputPageState extends State<InputPage> {
                   ),
                 ),
                 Expanded(
-                    child: ReusableCard(
-                  color: activeCardColor,
-                  cardChild: IconContent(
-                    icon: FontAwesomeIcons.venus,
-                    label: 'Female',
+                  child: ReusableCard(
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
+                    },
+                    color: selectedGender == Gender.female ? activeCardColor : inactiveCardColor,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      label: 'Female',
+                    ),
                   ),
-                )),
+                ),
               ],
             ),
           ),
