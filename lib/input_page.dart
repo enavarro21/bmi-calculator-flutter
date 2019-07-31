bce728d0acc73691b6fc8e7977a42ed8153edd55
Erwin Navarro <erwin.r.navarro1@gmail.com>
2019-07-31 13:41:14 -0700

    "Updated app layout, refactor"

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-import 'icon_content.dart';
-import 'reusable_card.dart';
 import 'constants.dart';
+import 'reusable_card.dart';
+import 'icon_content.dart';
+import 'round_icon_button.dart';
 
 enum Gender {
   male,
@@ -218,28 +219,6 @@ class _InputPageState extends State<InputPage> {
           ),
         ],
       ),
-    );
-  }
-}
-
-class RoundIconButton extends StatelessWidget {
-  RoundIconButton({@required this.icon, @required this.onPressed});
-
-  final IconData icon;
-  final Function onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-      child: Icon(icon),
-      onPressed: onPressed,
-      elevation: 6.0,
-      constraints: BoxConstraints.tightFor(
-        width: 56.0,
-        height: 56.0,
-      ),
-      shape: CircleBorder(),
-      fillColor: Color(0xFF4C4F5E),
     );
   }
 }
